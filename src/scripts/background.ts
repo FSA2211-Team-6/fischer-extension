b7801a274f1ff0bec24f6a3f846dbf47ce68d584
Winter Hardcastle <winter@hardcastle.io>
2023-02-08 16:52:39 -0500

    Calling API with existing route

--- src/scripts/background.ts
+++ src/scripts/background.ts
@@ -1,10 +1,6 @@
 // eslint-disable-next-line no-console
 console.log("background script");
 
-window.onload = () => {
-  getExistingAssertions();
-};
-
 const getActiveTabURL = async () => {
   const tabs: Array<any> = await chrome.tabs.query({
     currentWindow: true,
@@ -14,22 +10,11 @@ const getActiveTabURL = async () => {
   return tabUrl;
 };
 
-const getExistingAssertions = async () => {
-  const url = await getActiveTabURL();
-  const urlObj: any = { url: url.href };
-  const assertions: any = await fetch("http://localhost:3000/api/postURL", {
-    method: "POST",
-    body: urlObj,
-  });
-  //eslint-disable-next-line no-console
-  console.log(assertions);
-};
-
 const getSelectedText = async (info: chrome.contextMenus.OnClickData) => {
   const selectedText: any = info.selectionText;
 
   const url = await getActiveTabURL();
-  const urlHost = url.host;
+  const urlHost = url.origin;
   const urlPath = url.href;
 
   await chrome.storage.session.set({ selectedText: selectedText });
@@ -40,35 +25,39 @@ const getSelectedText = async (info: chrome.contextMenus.OnClickData) => {
 
 //QUERIES ChatGPT API AND ADDS NEW POST TO DATABASE
 const postAssertion = async (assertion: string, urlHost: string, urlArticle: string) => {
-  const aiResponse = await fetch("http://localhost:3000/api/factcheck", {
-    method: "POST",
-    body: assertion,
-  });
-  const data = await aiResponse.json();
-  const aiData: any = {
-    post: {
-      assertion: assertion,
-      aiResponse: data.choices[0].text,
-      userId: 1,
-      topic: data.choices[1].text,
-    },
-    website: {
-      host: urlHost,
-      article: urlArticle,
-    },
-  };
-  const aiDataJSON = JSON.stringify(aiData);
-  // eslint-disable-next-line no-console
-  console.log("AI DATA:", aiData);
-  // eslint-disable-next-line no-console
-  console.log("AI DATA JSON", aiDataJSON);
+  try {
+    const aiResponse = await fetch("http://localhost:3000/api/factcheck", {
+      method: "POST",
+      body: assertion,
+    });
+    const data = await aiResponse.json();
+    const aiData: any = {
+      post: {
+        assertion: assertion,
+        aiResponse: data.choices[0].text,
+        userId: 1,
+        topic: data.choices[1].text,
+      },
+      website: {
+        host: urlHost,
+        article: urlArticle,
+      },
+    };
+    const aiDataJSON = JSON.stringify(aiData);
+    // eslint-disable-next-line no-console
+    console.log("AI DATA:", aiData);
+    // eslint-disable-next-line no-console
+    console.log("AI DATA JSON", aiDataJSON);
 
-  const dbUpdate = await fetch("http://localhost:3000/api/post", {
-    method: "POST",
-    body: aiDataJSON,
-  });
-  //eslint-disable-next-line no-console
-  console.log(dbUpdate);
+    const dbUpdate = await fetch("http://localhost:3000/api/post", {
+      method: "POST",
+      body: aiDataJSON,
+    });
+    //eslint-disable-next-line no-console
+    console.log(dbUpdate);
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 //WHEN CONTEXT MENU ITEM IS CLICKED
--- src/scripts/content.ts
+++ src/scripts/content.ts
@@ -6,6 +6,40 @@ import type { Message } from "src/types";
 // eslint-disable-next-line no-console
 console.log("content script");
 
+// const getActiveTabURL = async () => {
+//   //eslint-disable-next-line no-console
+//   console.log("inside active tab func");
+//   const tabs: Array<any> = await chrome.tabs.query({
+//     currentWindow: true,
+//     active: true,
+//   });
+//   const tabUrl = new URL(tabs[0].url);
+//   //eslint-disable-next-line no-console
+//   console.log(tabUrl);
+//   return tabUrl;
+// };
+
+const getExistingAssertions = async () => {
+  const urlOBJ: any = { url: location.href };
+  const urlJSON = JSON.stringify(urlOBJ);
+  // const urlObj: any = { url: "https://javascript.info/url" };
+  //eslint-disable-next-line no-console
+  console.log(urlJSON);
+  try {
+    const assertions = await fetch("http://localhost:3000/api/postURL", {
+      method: "POST",
+      body: urlJSON,
+    });
+    const data = await assertions.json();
+    //eslint-disable-next-line no-console
+    console.log("Assertions: ", data);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+getExistingAssertions();
+
 // If you want to get the DOM of the open page, you can do it here.
 // document.querySelector("#some-id");
 chrome.runtime.onMessage.addListener((message: Message) => {
